73ddc433b116f8e7fba8b840a9d931d4f8433458
Koji E. Kosugi <kosugitti@gmail.com>
2023-06-05 08:56:08 +0900

    Trial writing of iterative estimation code

--- tests/testthat/Ch03Reliability_dev.R
+++ tests/testthat/Ch03Reliability_dev.R
@@ -45,14 +45,18 @@ alphaIfDel(V)
 
 V <- cov(tmp$U, use = "pairwise")
 V <- Exametrika::TetrachoricCorrelationMatrix(tmp$U)
+
 Esys <- eigen(V)
 Eval <- Esys$values[1]
 Evec <- Esys$vectors[, 1]
 Load <- sqrt(Eval) * Evec
+V <- Load %*% t(Load)
+diag(V) <- 0
+
 aj2 <- sum(Load)^2
 e2 <- sum(1 - diag(Load %*% t(Load)))
 aj2 / (aj2 + e2)
-# psych::omega(V)
+psych::omega(V)
 # psych::omega(tmp$U)
 result.fa <- psych::fa.poly(tmp$U, nfactors = 1)
 faLoads <- result.fa$fa$loadings %>% as.vector()
@@ -60,7 +64,6 @@ mean(Load / faLoads)
 e2 <- 1 - result.fa$fa$communality
 
 sum(faLoads^2) / (sum(faLoads^2) + sum(e2))
-
 
 
 omegaCoefficient <- function(V) {
@@ -91,3 +94,28 @@ V <- PhiCoefficient(U = tmp$U)
 omegaCoefficient(V)
 V <- TetrachoricCorrelationMatrix(U = tmp$U)
 omegaCoefficient(V)
+
+eps <- 1e-15
+FLG <- TRUE
+iterMax <- 100
+iter <- 0
+Vtmp <- V
+Ltmp <- rep(1,20)
+while(FLG & iter< iterMax){
+  iter <- iter + 1
+  Esys <- eigen(Vtmp)
+  Eval <- Esys$values[1]
+  Evec <- Esys$vectors[, 1]
+  Load <- sqrt(Eval) * Evec
+  Load <- sign(sum(sign(Load))) * Load
+  Vtmp2 <- Load %*% t(Load)
+  print(paste(iter,Eval))
+  if(sum((Load-Ltmp)^2) > eps){
+    Vtmp <- V
+    Ltmp <- Load
+    diag(Vtmp) <- diag(Vtmp2)
+  }else{
+    FLG = FALSE
+  }
+}
+
