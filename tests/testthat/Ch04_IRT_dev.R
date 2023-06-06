84ec3f2e89f4eed49ac0f11397862f50f348d426
kosugitti <kosugitti@gmail.com>
2023-06-06 16:22:27 +0900

    Create a numerical integration on my own. Using the Trapezoidal Rule.

--- tests/testthat/Ch04_IRT_dev.R
+++ tests/testthat/Ch04_IRT_dev.R
@@ -55,7 +55,7 @@ df <- data.frame(theta = x) %>%
   )
 df %>%
   pivot_longer(-theta) %>%
-  ggplot(aes(x = theta, y = value,color=name)) +
+  ggplot(aes(x = theta, y = value, color = name)) +
   geom_point()
 
 
@@ -93,7 +93,7 @@ optimize(
 # GPT4 refined!--------------------------------------------------------------------
 
 PL <- function(lambda, theta) {
-  p <- lambda[,3] + (lambda[,4] - lambda[,3]) / (1 + exp(-lambda[,1] * (theta - lambda[,2])))
+  p <- lambda[, 3] + (lambda[, 4] - lambda[, 3]) / (1 + exp(-lambda[, 1] * (theta - lambda[, 2])))
   return(p)
 }
 
@@ -105,7 +105,7 @@ LLtheta <- function(theta, Lambda, u, z) {
 
 LLthetaMAP <- function(theta, Lambda, u, z) {
   p <- PL(Lambda, theta)
-  ll <- sum(z * (u * log(p) + (1 - u) * log(1 - p))) - theta^2/2
+  ll <- sum(z * (u * log(p) + (1 - u) * log(1 - p))) - theta^2 / 2
   return(ll)
 }
 
@@ -129,58 +129,92 @@ result <- mapply(
 
 
 # EAP ---------------------------------------------------------------------
-pr <- function(theta,Lambda,u,z){
+pr <- function(theta, Lambda, u, z) {
   tmp <- 0
-  for(j in 1:NROW(Lambda)){
-    p <- Lambda[j,3] + (Lambda[j,4] - Lambda[j,3]) / (1 + exp(-Lambda[j,1] * (theta - Lambda[j,2])))
-    tmp <- tmp + u[j] *log(p) + (1-u[j])*log(1-p)
+  for (j in 1:NROW(Lambda)) {
+    p <- Lambda[j, 3] + (Lambda[j, 4] - Lambda[j, 3]) / (1 + exp(-Lambda[j, 1] * (theta - Lambda[j, 2])))
+    tmp <- tmp + u[j] * log(p) + (1 - u[j]) * log(1 - p)
   }
-  ll <- tmp- theta^2/2
+  ll <- tmp - theta^2 / 2
   llexp <- exp(ll)
   return(llexp)
 }
 
-prMat <- function(theta, Lambda, u, z){
-  p <- Lambda[,3] + (Lambda[,4] - Lambda[,3]) / (1 + exp(-Lambda[,1] * (theta - Lambda[,2])))
-  ll <- sum(u * log(p) + (1-u) * log(1-p)) - theta^2/2
-  llexp <- exp(ll)
-  return(llexp)
+prMat <- function(theta, Lambda, u, z) {
+  f <- function(x) {
+    p <- Lambda[, 3] + (Lambda[, 4] - Lambda[, 3]) / (1 + exp(-Lambda[, 1] * (x - Lambda[, 2])))
+    ll <- sum(u * log(p) + (1 - u) * log(1 - p)) - x^2 / 2
+    llexp <- exp(ll)
+    return(llexp)
+  }
+  f_vec <- Vectorize(f)
+  return(f_vec(theta))
 }
 
-pr(result[1],Lambda,U[1,],Z[1,])
-prMat(result[1],Lambda,U[1,],Z[1,])
+pracma::quadv(function(x) prMat(x, Lambda, u = U[1, ], z = Z[1, ]), -5, 5)
+integrate(function(x) pr(theta = x, Lambda = Lambda, u = U[1, ], z = Z[1, ]), lower = -5, upper = 5)$value
 
-pracma::quadv(function(x) prMat(x,Lambda,u=U[1,],z=Z[1,]),-5,5)
-integrate(function(x) pr(theta=x,Lambda=Lambda,u=U[1,],z=Z[1,]),lower=-5,upper=5)$value
-
-for(s in 1:NROW(U)){
-  u <- U[s,]
-  z <- Z[s,]
-  denomi <- integrate(function(x) pr(theta=x,Lambda=Lambda,u=u,z=z),lower=-5,upper=5)$value
-  numer <- integrate(function(x) x*pr(theta=x,Lambda=Lambda,u=u,z=z),lower=-5,upper=5)$value
-  denomi2 <- pracma::quadv(function(x) prMat(x,Lambda,u=u,z=z),-5,5)$Q
-  numer2 <- pracma::quadv(function(x) x*prMat(x,Lambda,u=u,z=z),-5,5)$Q
-  print(paste(numer/denomi,numer2/denomi2))
+for (s in 1:NROW(U)) {
+  u <- U[s, ]
+  z <- Z[s, ]
+  denomi <- integrate(function(x) pr(theta = x, Lambda = Lambda, u = u, z = z), lower = -5, upper = 5)$value
+  numer <- integrate(function(x) x * pr(theta = x, Lambda = Lambda, u = u, z = z), lower = -5, upper = 5)$value
+  denomi2 <- pracma::quadv(function(x) prMat(x, Lambda, u = u, z = z), -5, 5)$Q
+  numer2 <- pracma::quadv(function(x) x * prMat(x, Lambda, u = u, z = z), -5, 5)$Q
+  print(paste(numer / denomi, numer2 / denomi2))
 }
 
-## 関数化
+## 関数化(PSDと一緒に)
 
-EAPs <- function(Lambda,U,Z){
+EAPs <- function(Lambda, U, Z) {
   Ns <- NROW(U)
-  ret <- vector(length=Ns)
-  PSD <- vector(length=Ns)
-  for(s in 1:Ns){
-    u <- U[s,]
-    z <- Z[s,]
-    denominator <- integrate(function(x) pr(theta=x,Lambda=Lambda,u=u,z=z),lower=-5,upper=5)$value
-    numerator <- integrate(function(x) x*pr(theta=x,Lambda=Lambda,u=u,z=z),lower=-5,upper=5)$value
-    ret[s] <- numerator/denominator
-    tmp <- integrate(function(x) (x-ret[s])^2*pr(theta=x,Lambda=Lambda,u=u,z=z),lower=-5,upper=5)$value
-    PSD[s] <- sqrt(tmp/denominator)
+  ret <- vector(length = Ns)
+  PSD <- vector(length = Ns)
+  for (s in 1:Ns) {
+    u <- U[s, ]
+    z <- Z[s, ]
+    denominator <- integrate(function(x) pr(theta = x, Lambda = Lambda, u = u, z = z), lower = -5, upper = 5)$value
+    numerator <- integrate(function(x) x * pr(theta = x, Lambda = Lambda, u = u, z = z), lower = -5, upper = 5)$value
+    ret[s] <- numerator / denominator
+    tmp <- integrate(function(x) (x - ret[s])^2 * pr(theta = x, Lambda = Lambda, u = u, z = z), lower = -5, upper = 5)$value
+    PSD[s] <- sqrt(tmp / denominator)
   }
-  ret = list(ret,PSD)
+  ret <- list(ret, PSD)
   return(ret)
 }
 
-EAPs(Lambda,U,Z)
+EAPs(Lambda, U, Z)
 
+# 積分関数を自作して ---------------------------------------------------------------
+
+prMat2 <- function(theta, Lambda, U, Z) {
+  f <- function(x) {
+    p <- Lambda[, 3] + (Lambda[, 4] - Lambda[, 3]) / (1 + exp(-Lambda[, 1] * (x - Lambda[, 2])))
+    ll <- sum(u * log(p) + (1 - u) * log(1 - p)) - x^2 / 2
+    ll <- U %*% log(p) + (1 - U) %*% log(1 - p) - x^2 / 2
+    return(exp(ll))
+  }
+  f_vec <- Vectorize(f)
+  return(f_vec(theta))
+}
+
+EAP_PSD <- function(Lambda, U, Z) {
+  dev <- 0.01
+  xdev <- seq(-5, 5, dev)
+  N <- length(xdev)
+
+  tmp1 <- prMat2(xdev[1:N - 1], Lambda, U, Z)
+  tmp2 <- prMat2(xdev[2:N], Lambda, U, Z)
+
+  denominator <- rowSums((tmp1 + tmp2) * dev / 2)
+  numerator <- rowSums((tmp1 %*% diag(xdev[1:N - 1]) + tmp2 %*% diag(xdev[2:N])) * dev / 2)
+  EAP <- numerator / denominator
+
+  weight <- ((EAP %*% t(rep(1, length(xdev)))) - ((rep(1, length(EAP))) %*% t(xdev)))^2
+  numerator <- rowSums((tmp1 * weight[, 1:N - 1] + tmp2 * weight[, 2:N]) * dev / 2)
+  PSDs <- sqrt(numerator / denominator)
+
+  return(list(EAP = EAP, PSDs = PSDs))
+}
+
+EAP_PSD(Lambda, U, Z)
