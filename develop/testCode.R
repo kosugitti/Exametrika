c165a6b36dda68777672d2a0d1bd847db1b9140d
Koji E. Kosugi <kosugitti@gmail.com>
2023-07-23 17:28:08 +0900

    dev folder del

--- develop/testCode.R
+++
@@ -1,5 +0,0 @@
-# library(Exametrika)
-# testthat::test_file("test-chapter02.R")
-# testthat::test_file("test-ItemTest.R")
-# testthat::test_file("test-TestAnalysis.R")
-# testthat::test_file("test-ch4irt.R")
